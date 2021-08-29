f82a6431bfe8f84f9b4d636a14105beee1137203
weTem <89235051+weTem@users.noreply.github.com>
2021-08-29 17:05:59 +0530

    Update ping.py

--- SpamBots/plugins/ping.py
+++ SpamBots/plugins/ping.py
@@ -36,16 +36,16 @@ def get_readable_time(seconds: int) -> str:
 
     return ping_time
 
-@UstaD.on(events.NewMessage(pattern="/ping"))
-@UstaD2.on(events.NewMessage(pattern="/ping"))
-@UstaD3.on(events.NewMessage(pattern="/ping"))
-@UstaD4.on(events.NewMessage(pattern="/ping"))
-@UstaD5.on(events.NewMessage(pattern="/ping"))
-@UstaD6.on(events.NewMessage(pattern="/ping"))
-@UstaD7.on(events.NewMessage(pattern="/ping"))
-@UstaD8.on(events.NewMessage(pattern="/ping"))
-@UstaD9.on(events.NewMessage(pattern="/ping"))
-@UstaD10.on(events.NewMessage(pattern="/ping"))
+@UstaD.on(events.NewMessage(pattern=".ping"))
+@UstaD2.on(events.NewMessage(pattern=".ping"))
+@UstaD3.on(events.NewMessage(pattern=".ping"))
+@UstaD4.on(events.NewMessage(pattern=".ping"))
+@UstaD5.on(events.NewMessage(pattern=".ping"))
+@UstaD6.on(events.NewMessage(pattern=".ping"))
+@UstaD7.on(events.NewMessage(pattern=".ping"))
+@UstaD8.on(events.NewMessage(pattern=".ping"))
+@UstaD9.on(events.NewMessage(pattern=".ping"))
+@UstaD10.on(events.NewMessage(pattern=".ping"))
 async def ping(e):
     if e.sender_id in SMEX_USERS:
         start = datetime.now()
